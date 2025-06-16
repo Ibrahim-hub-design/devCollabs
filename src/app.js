9fffd74d13bba01701587b4141905d6c47c0d575
Apple <apple@Apple-ka-Air.bbrouter>
2025-06-16 21:58:35 +0530

    created an express server

--- src/app.js
+++ src/app.js
@@ -2,11 +2,8 @@ const express = require("express");
 
 const app = express();
 
-app.use("/info", (req, res) => {
-  res.send("Hey buddies+");
-  console.log("I am active");
+app.use("/test", (req, res) => {
+  res.send("Hey Im working");
 });
 
-app.listen(3000, () => {
-  console.log("Server is ready");
-});
+app.listen(3000);
